0d9df1063cc9aaf9f5b0308ae7d9f5458ed8b5d9
ELHARFALI <elharfali.youssef.20@gmail.com>
2022-07-31 19:09:41 +0100

    Courses Section Slider

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
-const menuBtn = document.getElementById('menu-btn')
+const menuBtn = document.getElementById("menu-btn");
 const navbar = document.querySelector(".navbar");
 
 const menuButton = function () {
-    menuBtn.classList.toggle('fa-times')
-    navbar.classList.toggle('active')
-}
+  menuBtn.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-menuBtn.addEventListener('click', menuButton)
+menuBtn.addEventListener("click", menuButton);
+
+var swiper = new Swiper(".course-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+        slidesPerView: 1,
+        },
+        768: {
+        slidesPerView: 2,
+        },
+        1024: {
+        slidesPerView: 3,
+        },
+    },
+});
